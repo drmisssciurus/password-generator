19ef6e9bac8bde98cf6f007956c180f95c23c237
Alina <ms.sciurus@gmail.com>
2024-09-17 11:13:46 +0300

    add some styles and add some js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello There');
+const passwordLength = document.getElementById("password-length"),
+      button = document.getElementById("button"),
+      numbers = document.getElementById("numbers"),
+      lowercaseCharacters = document.getElementById("lowercase-characters"),
+      uppercaseharacters = document.getElementById("uppercase-characters"),
+      newPassword = document.getElementById("new-password");
+
+
+console.log(passwordLength.value);

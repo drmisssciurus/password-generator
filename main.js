63fb0e7f15a495700ce61377e46c07c802a6b2ab
Alina <ms.sciurus@gmail.com>
2024-09-22 21:54:19 +0300

    add some utils functions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const passwordLength = document.getElementById("password-length"),
+const userSelect = document.getElementById("password-length"),
       buttonPassMade = document.getElementById("button"),
       numbers = document.getElementById("numbers"),
       lowercaseCharacters = document.getElementById("lowercase-characters"),
@@ -6,9 +6,29 @@ const passwordLength = document.getElementById("password-length"),
       newPassword = document.getElementById("new-password");
 
 
-console.log(passwordLength.value);
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(typeof +userSelect.value); //!!!!!
+
+
+//function for sybmols
+
+// function getRandomSymbol() {
+//     const symbols = "!@#$%^&*()_+[]{}|;:',.<>?/~`-=";
+//     return symbols[Math.floor(Math.random() * symbols.length)]
+// }
+
+const passwordLength = userSelect.value;
 buttonPassMade.addEventListener('click', ()=> {
-    console.log('Hello There');
-    
+    console.log(Math.floor(Math.random() * 20));
 });

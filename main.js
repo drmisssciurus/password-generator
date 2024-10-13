b38faf74d6fb450a4647f174aa9286afffa7465d
Alina <ms.sciurus@gmail.com>
2024-10-13 20:33:16 +0300

    add generate password functions

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const userSelect = document.getElementById("password-length"),
       numbers = document.getElementById("numbers"),
       lowercaseCharacters = document.getElementById("lowercase-characters"),
       uppercaseCharacters = document.getElementById("uppercase-characters"),
-      buttonPassMade = document.getElementById("button"),
+      buttonPassGenerate = document.getElementById("button"),
       newPassword = document.getElementById("new-password");
 
 //obj
@@ -34,18 +34,55 @@ function getRandomNumber() {
 //     return symbols[Math.floor(Math.random() * symbols.length)]
 // }
 
+// Generate event listen
 
-// button listener
-
-
-buttonPassMade.addEventListener('click', () => {
+buttonPassGenerate.addEventListener('click', () => {
     const passwordLength = +userSelect.value;
 
     const hasNumber = numbers.checked;
     const hasLower = lowercaseCharacters.checked;
     const hasUpper = uppercaseCharacters.checked;
-
-
-    console.log(hasNumber, hasLower, hasUpper, passwordLength);
-    
+    // use value for input but for div u should use innerText or innerHTML
+    newPassword.value = generatePassword(hasNumber, hasLower, hasUpper, passwordLength);
 });
+
+// Generate password function
+
+const generatePassword = (number, lower, upper, length) => {
+    //1. Init passw var
+    let generatedPassword = '';
+    const typesCount = number + lower + upper;
+
+    // console.log('typesCount: ', typesCount);
+    
+    const typesArr = [{ number }, { lower }, { upper }]
+    .filter(
+        item => Object.values(item)[0]
+    );
+
+    // console.log('typesArr: ', typesArr);
+
+    //2. Filter out unchecked types
+
+    if(typesCount === 0) {
+        return '';
+    }
+
+    //3. Loop ower lenght call generator funk for each type
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+
+            // console.log('funcName: ', funcName);
+
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+
+    //to slice it if u need short password
+    // generatedPassword.slice(0, length);
+
+    //4.  return new passw
+    return generatedPassword;
+}

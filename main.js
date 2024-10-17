446f217d06bd862528bf5387c3793890cc61312d
Alina <ms.sciurus@gmail.com>
2024-10-17 15:48:36 +0300

    add symbols and add some styles

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const userSelect = document.getElementById("password-length"),
       numbers = document.getElementById("numbers"),
       lowercaseCharacters = document.getElementById("lowercase-characters"),
       uppercaseCharacters = document.getElementById("uppercase-characters"),
+      symbolsCharacters = document.getElementById("symbols-characters"),
       buttonPassGenerate = document.getElementById("button"),
       newPassword = document.getElementById("new-password");
 
@@ -10,7 +11,8 @@ const userSelect = document.getElementById("password-length"),
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
-    number: getRandomNumber
+    number: getRandomNumber,
+    symbol: getRandomSymbol
 };
 
 // Utils functions
@@ -29,10 +31,10 @@ function getRandomNumber() {
 
 //function for sybmols
 
-// function getRandomSymbol() {
-//     const symbols = "!@#$%^&*()_+[]{}|;:',.<>?/~`-=";
-//     return symbols[Math.floor(Math.random() * symbols.length)]
-// }
+function getRandomSymbol() {
+    const symbols = "!@#$%^&*()_+[]{}|;:',.<>?/~`-=";
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
 
 // Generate event listen
 
@@ -42,20 +44,21 @@ buttonPassGenerate.addEventListener('click', () => {
     const hasNumber = numbers.checked;
     const hasLower = lowercaseCharacters.checked;
     const hasUpper = uppercaseCharacters.checked;
+    const hasSymbols = symbolsCharacters.checked;
     // use value for input but for div u should use innerText or innerHTML
-    newPassword.value = generatePassword(hasNumber, hasLower, hasUpper, passwordLength);
+    newPassword.value = generatePassword(hasNumber, hasLower, hasUpper, hasSymbols, passwordLength);
 });
 
 // Generate password function
 
-const generatePassword = (number, lower, upper, length) => {
+const generatePassword = (number, lower, upper, symbol, length) => {
     //1. Init passw var
     let generatedPassword = '';
-    const typesCount = number + lower + upper;
+    const typesCount = number + lower + upper + symbol;
 
     // console.log('typesCount: ', typesCount);
     
-    const typesArr = [{ number }, { lower }, { upper }]
+    const typesArr = [{ number }, { lower }, { upper }, { symbol }]
     .filter(
         item => Object.values(item)[0]
     );
@@ -86,3 +89,5 @@ const generatePassword = (number, lower, upper, length) => {
     //4.  return new passw
     return generatedPassword;
 }
+
+console.log('Hello There! My name is Alina! Here is my Password Generator app where I learn how to write vanilla JS and work with objects and arrays. If you are interested in giving me a job, feel free to contact me: https://www.linkedin.com/in/alina-levintas/ \nMay The Force be with you!');
